7be198626d6fc4e23130cc127c03d07a6f1622fb
Nugni <61407756+Nugni@users.noreply.github.com>
2023-02-20 14:51:47 +0100

    Made simple trainingloop and visualization function. Not tested

--- src/trainingFunctionality.py
+++ src/trainingFunctionality.py
@@ -1,7 +1,77 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#NOT TESTED
+
+#Function that given relevant parameters perform a training loop of some net
+#returns 2 lists of training and test loss for plotting loss as a function
+#of training iterations
+def trainLoop(net, optimizer, criterion, device, epochs, train_loader, test_loader, print_interv=5):
+    train_loss_lst = []
+    test_loss_lst = []
+    for epoch in range(epochs):
+        run_train_loss = 0.0
+        run_test_loss = 0.0
+        #iterate over training dataset
+        for i, data in enumerate(train_loader, 0):
+            inputs, labs = data
+
+            #transfer data to device, e.g. GPU
+            inputs, labs = inputs.to(device), labs.to(device)
+
+            #maybe
+            #inputs = inputs[0]. prolly not. 
+
+            #zero the parameter gradients
+            optimizer.zero_grad()
+
+            #forward + backward + optimize
+            outputs = net(inputs)
+
+            loss = criterion(input=outputs, target=labs)
+            loss.backward()
+            optimizer.step()
+
+            #print statistics:
+            run_train_loss = loss.item()
+            if i % print_interv == 9: #print every 10 minibatches
+                print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, run_train_loss / print_interv))
+                train_loss_lst.append()
+                running_loss = 0.0
+
+                #compute some test error
+                test_input, test_lab = iter(test_loader).next()
+
+                #move to device
+                test_input, test_lab = test_input.to(device), test_lab.to(device)
+
+                test_out = net(test_input)
+
+                loss_test = criterion(input=test_out, target=test_lab)
+                run_test_loss = loss_test.item()
+
+                test_loss_lst.append(run_test_loss)
+                run_test_loss = 0.0
+        print("Finished training! :^)")
+        return train_loss_lst, test_loss_lst, net
 
 
-def trainLoop(model, net, epochs, optimizer, dataset):
-    #do something
-    #train
-    #visualize and print parameters
-    #maybe save graphs
+#NOT TESTED
+#function to plot traing loss and possibly test loss after training
+def visualizeLoss(print_interv, train_lst, test_lst=None):
+    xTrain = (np.arange(len(train_lst))+1)*print_interv
+    yTrain = train_lst
+    plt.plot(xTrain, yTrain, label='Train loss')
+
+    #Add test loss to plot if wished
+    if test_lst is not None:
+        xTest = (np.arange(len(test_lst))+1)*print_interv
+        yTest = test_lst
+        plt.plot(xTest, yTest, label='Test loss')
+
+    #Add text to plot
+    plt.xlabel("Training iterations")
+    plt.ylabel("Loss")
+    plt.title("Loss of model over number of iterations")
+    plt.legend() #should plot which graph is which
+    plt.show()
