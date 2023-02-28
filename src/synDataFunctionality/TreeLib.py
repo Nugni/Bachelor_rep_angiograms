a81d64f45459146ac47c2f6f9e7245a0aa0f1876
Nugni <61407756+Nugni@users.noreply.github.com>
2023-02-28 17:45:22 +0100

    changed method for estimating blood vessel length

--- src/synDataFunctionality/TreeLib.py
+++ src/synDataFunctionality/TreeLib.py
@@ -5,10 +5,19 @@ from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+def genNewLength(length, lengthRatioMean, lengthRatiostd):
+    ratio = np.random.normal(lengthRatioMean, lengthRatiostd)
+    while ratio < 0.3: #blood vessels do not really become smaller than this
+        ratio = np.random.normal(lengthRatioMean, lengthRatiostd)
+    newLen = ratio*length
+    if newLen < 10: #The lengths do not become smaller than 10
+        newLen = 10
+    #print("ratio {0}, length {1}".format(ratio, length))
+    #print(ratio*length)
+    return newLen
 #Synthetic angogram class: Node
 class Node:
-    def __init__(self, coord, width, pLength, angle, bifurcProb, bifurcBigLeft, stdMul):
+    def __init__(self, coord, width, pLength, angle, bifurcProb, bifurcBigLeft, stdMul, lengthRatioMean, lengthRatiostd):
         self.coord = coord
         self.width = width
         self.pLength = pLength
@@ -16,18 +25,20 @@ class Node:
         self.stdMul = stdMul
         self.bifurcProb = bifurcProb
         self.bifurcBigLeft = bifurcBigLeft
+        self.lengthRatioMean = lengthRatioMean
+        self.lengthRatiostd = lengthRatiostd
         self.children = []
 
     #Creates a child node
-    def createChild(self, angle, ratio):#, roadtravelled):
-        newWidth = self.width*ratio
-        newLength = self.pLength*ratio#(self.pLength + roadtravelled)*ratio 
+    def createChild(self, angle, ratioWidth):#, roadtravelled):
+        newWidth = self.width*ratioWidth
+        newLength = genNewLength(self.pLength, self.lengthRatioMean, self.lengthRatiostd)  #(self.pLength + roadtravelled)*ratio 
         curX = self.coord[0]
         curY = self.coord[1]
         newX = curX + math.cos(angle)*newLength
         newY = curY + math.sin(angle)*newLength
         newCoord = newX, newY
-        return Node(newCoord, newWidth, newLength, angle, self.bifurcProb, self.bifurcBigLeft, self.stdMul)
+        return Node(newCoord, newWidth, newLength, angle, self.bifurcProb, self.bifurcBigLeft, self.stdMul, self.lengthRatioMean, self.lengthRatiostd)
 
     #Creates and add either 1 or 2 children to the parent node.
     def addChildren(self):
@@ -48,20 +59,23 @@ class Node:
             self.children.append(rightChild)
         else:
             #Make 1 child following an angle
-            singleChild = self.createChild(random.gauss(self.angle, self.angle*self.stdMul), 1)#, self.pLength)
+            singleChild = self.createChild(
+                random.gauss(self.angle, self.angle*self.stdMul),
+                0.98
+                )#, self.pLength)
             self.children.append(singleChild)
 
 
 #Synthetic Angiogram class: Tree
 class Tree:
-    def __init__(self, x, y, width, pLength, angle, stopWidth, bifurcProb=0.3, bifurcBigLeft = 0.5, angleStdMul = 0.0):
+    def __init__(self, x, y, width, pLength, angle, stopWidth, angleStdMul = 0.0, bifurcProb=0.3, bifurcBigLeft = 0.5, lengthRatioMean = 0.95, lengthRatiostd=0.43):
         self.stopWidth = stopWidth
         self.angleStdMul = angleStdMul
-        self.addRoot(x,y, width, pLength, angle, bifurcProb, bifurcBigLeft)
+        self.addRoot(x,y, width, pLength, angle, bifurcProb, bifurcBigLeft, lengthRatioMean, lengthRatiostd)
         self.makeTree()
 
-    def addRoot(self, x, y, width, pLength, angle, bifurcProb, bifurcBigLeft):
-        self.root = Node((x,y), width, pLength, angle, bifurcProb, bifurcBigLeft, self.angleStdMul)
+    def addRoot(self, x, y, width, pLength, angle, bifurcProb, bifurcBigLeft, lengthRatioMean, lengthRatiostd):
+        self.root = Node((x,y), width, pLength, angle, bifurcProb, bifurcBigLeft, self.angleStdMul, lengthRatioMean, lengthRatiostd)
     
     def growTree(self, node):
         if node.width < self.stopWidth:
